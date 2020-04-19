89031e4f3c34d48c1dd22ad24790a2151f6c9a64
GBD <Dukarevich_G@EplusE.ru>
2020-04-19 16:21:34 +0300

    Adds comments to BollToStringConverter

--- XF_FirstRun/XF_FirstRun/Helpers/BoolToStringConverter.cs
+++ XF_FirstRun/XF_FirstRun/Helpers/BoolToStringConverter.cs
@@ -6,16 +6,41 @@ using Xamarin.Forms;
 
 namespace XF_FirstRun.Helpers
 {
+    /// <summary>
+    /// Конвертор (преобразователь) логических значейний (bool) в строки "true" и "false" соответственно
+    /// и обратно.
+    /// </summary>
+    /// <remarks>
+    /// При обратном преобразовании значение "true" (любыми буквами) преобразуется в "истину" 
+    /// все остальные значения в "ложь".
+    /// </remarks>
     public class BoolToStringConverter : IValueConverter
     {
+        /// <summary>
+        /// Преобразовывает логическое значение типа bool в строковое значение "true" или "false"
+        /// </summary>
+        /// <param name="value">Преобразовываемое значение</param>
+        /// <param name="targetType">не используется</param>
+        /// <param name="parameter">не используется</param>
+        /// <param name="culture">не используется</param>
+        /// <returns>Строка "true" или "false" в зависимости от значения value</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (bool)value ? "true" : "false";
         }
 
+        /// <summary>
+        /// Преобразует строку "true" (любыми буквами) в логическое (bool) значение "истина",
+        /// все остальные строки в значение "ложь"
+        /// </summary>
+        /// <param name="value">Преобразовываемое значение</param>
+        /// <param name="targetType">не используется<</param>
+        /// <param name="parameter">не используется<</param>
+        /// <param name="culture">не используется<</param>
+        /// <returns>Логическое (bool) значение в зависимости от значения value</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (string)value == "true";
+            return ((string)value).ToLower() == "true";
         }
     }
 }
