e2dda6ddad052856b555b212f743ba0deb2f4ccb
GBD <Dukarevich_G@EplusE.ru>
2020-05-01 23:14:30 +0300

    Event-to-Command works.

---
+++ XF_FirstRun/XF_FirstRun/Behaviors/BaseBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF_FirstRun.Behaviors
+{
+    /// <summary>
+    /// Базовый класс для создания "поведений" (Behavior)
+    /// </summary>
+    /// <typeparam name="T">Класс к которому добавляется "поведение"</typeparam>
+    abstract public class BaseBehavior<T> : Behavior<T> where T : BindableObject
+    {
+        
+        /// <summary>
+        /// Объест к которому добавляется "поведение"
+        /// </summary>
+        public T AssociatedObject { get; private set; }
+
+        protected override void OnAttachedTo(T bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+            bindable.BindingContextChanged += OnBindableContextChanged;
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindableContextChanged;
+            AssociatedObject = null;
+        }
+        void OnBindableContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+    }
+}
